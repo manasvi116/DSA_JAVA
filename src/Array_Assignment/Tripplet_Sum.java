1c1f7b0248a2812a0ba1101a560ca31ee6371f7e
manasvi arora <manasvi925@gmail.com>
2025-04-24 10:56:35 +0530

    ARRAY

--- src/Array_Assignment/Tripplet_Sum.java
+++ src/Array_Assignment/Tripplet_Sum.java
@@ -7,7 +7,7 @@ public class Tripplet_Sum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int target =5;
+		int target =8;
 		Scanner sc =new Scanner (System.in);
 		System.out.println("enter size");
 		int size = sc.nextInt();
@@ -18,13 +18,35 @@ public class Tripplet_Sum {
 			arr[i] = sc.nextInt();
 		}
 		Arrays.sort(arr);
-		triplet_sum(arr , size);
-//		for(int i = 0; i <size ; i++) {
+		triplet_sum(arr , size , target);
 		
-//		}
+//		
 
 	}
-	public static int triplet_sum(int [] arr , int size) {
+	public static void triplet_sum(int [] arr , int size , int target) {
+		int sum = 0 ;
+		int i =0;
+		
+		for ( i=0 ; i < size-2 ; i++) {
+			int j = i+1;
+			int k = size-1;
+			while(j<k) {
+				sum = arr[i] + arr[j] + arr[k] ;
+				if(sum==target) {
+					System.out.println("(" + arr[i] +","+ arr[j]+ "," + arr[k] + ")");
+					j++;
+					k--;
+				}
+				else if(sum>target) {
+					k--;
+				}
+				else {
+					j++;
+				}
+			}
+		}
+		
+		
 		
 		
 	}
